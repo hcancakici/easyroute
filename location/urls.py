0af84bbe3b94a26115237b528504cdb2edd68e33
Furkan Kapukaya <furkan.kapukaya@akinon.com>
2020-12-21 18:56:35 +0300

    leaflet map template demo

--- location/urls.py
+++ location/urls.py
@@ -1,10 +1,14 @@
+from django.urls import include, path
 from rest_framework import routers
 
-from location import views 
+from location import views
 
 
 router = routers.DefaultRouter()
 router.register(r'', views.LocationViewSet)
 
-urlpatterns = router.urls
 
+urlpatterns = [
+    path('/', include(router.urls)),
+    path('map/', views.MapViewSet.as_view(), name="map-view"),
+]
--- location/views.py
+++ location/views.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 from location.serializers import LocationSerializer, LocationUserSerializer
 from location.models import Location, LocationUser
-
 
 
 class LocationUserViewSet(viewsets.ModelViewSet):
@@ -15,3 +18,14 @@ class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
     ordering_fields = '__all__'
+
+
+class MapViewSet(generics.RetrieveAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    template_name = "map2.html"
+    renderer_classes = [TemplateHTMLRenderer]
+    http_method_names = ['get', 'post']
+
+    def get(self, request, *args, **kwargs):
+        return Response({'data': "data"}, template_name="map2.html")
